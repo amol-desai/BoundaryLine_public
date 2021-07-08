cc67d498f3c7acaaf42d0cfe550da6a8dbffa934
amol-desai <amol.desai@gmail.com>
2021-07-08 16:05:14 -0700

    bug fix on over_ball, over_num swap as found by @omkarb1604

--- HawkEyeScrapers/hawkeye_scraper_helpers.py
+++ HawkEyeScrapers/hawkeye_scraper_helpers.py
@@ -63,8 +63,8 @@ def get_tracking_df_from_matchid(match_id):
     else:
       df['over'] = df.over.apply(lambda x: str(x).split('.'))
       df['match_inn'] = df.over.apply(lambda x: x[0])
-      df['over_ball'] = pd.to_numeric(df.over.apply(lambda x: x[1]), errors='coerce')
-      df['over_num'] = pd.to_numeric(df.over.apply(lambda x: x[2]), errors='coerce')
+      df['over_ball'] = pd.to_numeric(df.over.apply(lambda x: x[2]), errors='coerce')
+      df['over_num'] = pd.to_numeric(df.over.apply(lambda x: x[1]), errors='coerce')
       df.drop('over', axis=1, inplace=True)
 
       df['speed'] = pd.to_numeric(df['speed'], errors='coerce')*3.6
