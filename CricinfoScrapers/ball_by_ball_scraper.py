34b1278fe6dce2d612a9dc7ab6aeece7cd15196a
James Delaney <jd688@cam.ac.uk>
2021-10-27 22:01:32 +0100

    adding series_id from match_id function

--- CricinfoScrapers/ball_by_ball_scraper.py
+++ CricinfoScrapers/ball_by_ball_scraper.py
@@ -14,12 +14,10 @@ import json
 import helpers
 
 
-def get_ball_by_ball_from_match_id(match_id, include_commentary=True):
+def get_ball_by_ball_from_match_id(match_id, series_id='1', include_commentary=True):
     # Scrape ball by ball data from Cricinfo commentary for the match
     # Includes commentary text. This can be disabled to reduce data size
-
-    series_id = helpers.get_series_id_from_match_page(
-        helpers.get_match_url_from_match_id(match_id))
+    # actual series_id is optional and "1" works as of 2019-06-02
 
     url_template = "http://site.web.api.espn.com/apis/site/v2/sports/cricket/{series_id}/playbyplay?event={match_id}&page={page_num}"
 
--- CricinfoScrapers/helpers.py
+++ CricinfoScrapers/helpers.py
@@ -17,35 +17,44 @@ import numpy as np
 from time import sleep
 from datetime import datetime
 
+
 def get_soup_from_url(url):
-# Helper to get response soup from url
+    # Helper to get response soup from url
     sleep(1)
     try:
         html = urlopen(url).read()
     except HTTPError:
         print("Link Cannot be Reached")
-        return -1   
+        return -1
     #soup = BeautifulSoup(html,"lxml")
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
     return soup
 
+
 def get_id_from_url(url):
-# url usually ends with an id (match url has match_id, player url has player_id etc.)
+    # url usually ends with an id (match url has match_id, player url has player_id etc.)
     id = re.search('([0-9]+).html$', url)
     if id != None:
         return id[1]
     else:
         return 'NA'
-      
+
+
 def get_match_url_from_match_id(match_id):
-# This is specific to match url. This might be redundant with get_id_from_url, but this isn't tested
-# and needs replacement where used. So consider this legacy code until tested
-    url = "http://stats.espncricinfo.com/ci/engine/match/{match_id}.html".format(match_id=str(match_id))
+    # This is specific to match url. This might be redundant with get_id_from_url, but this isn't tested
+    # and needs replacement where used. So consider this legacy code until tested
+    url = "http://stats.espncricinfo.com/ci/engine/match/{match_id}.html".format(
+        match_id=str(match_id))
     return url
+
 
 def get_series_id_from_match_page(url):
     soup = get_soup_from_url(url)
-    series_link = [i.find('a') 
-                 for i in soup.find_all(class_='match-details-table') if 'Series' in i.text
-                ][0]['href']
+    series_link = [i.find('a')
+                   for i in soup.find_all(class_='match-details-table') if 'Series' in i.text
+                   ][0]['href']
     return re.search('([0-9]+)', series_link)[1]
+
+
+def get_series_id_from_match_id(match_id):
+    return get_series_id_from_match_page(get_match_url_from_match_id(match_id))
