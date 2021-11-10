7e2802e9f24fe223fee57f8a5802a33d8c3d787c
James Delaney <jd688@cam.ac.uk>
2021-11-10 12:00:27 +0000

    fixed issue with checking if bowling or batting first

--- CricinfoScrapers/player_data_scraper.py
+++ CricinfoScrapers/player_data_scraper.py
@@ -5,16 +5,15 @@ import pandas as pd
 ESPN_BOWLING_COLUMNS = ['Format', 'Mat', 'Inns', 'Balls', 'Runs', 'Wkts', 'BBI', 'BBM', 'Ave',
                         'Econ', 'SR', '4w', '5w', '10w']
 
-ESPN_BATTLING_COLUMNS = ['Format', 'Mat', 'Inns', 'NO', 'Runs', 'HS', 'Ave', 'BF', 'SR', '100s',
-                         '50s', '4s', '6s', 'Ct', 'St'],
+ESPN_BATTING_COLUMNS = ['Format', 'Mat', 'Inns', 'NO', 'Runs', 'HS', 'Ave', 'BF', 'SR', '100s',
+                        '50s', '4s', '6s', 'Ct', 'St'],
 
 
 def get_player_data_from_player_id(player_id):
     """
     Gets player data from player id
     """
-    player_data_url = "http://stats.espncricinfo.com/ci/engine/player/{}.html".format(
-        player_id)
+    player_data_url = f"http://stats.espncricinfo.com/ci/engine/player/{player_id}.html"
     soup = get_soup_from_url(player_data_url)
     player_data = {}
     player_data["player_id"] = player_id
@@ -23,12 +22,12 @@ def get_player_data_from_player_id(player_id):
     player_data["country"] = soup.find(
         "span", {"class": "player-card__country-name"}).text
     for header in soup.find("div", {"class": "player_overview-grid"}):
-        player_data[header.find("p")] = header.find("h5")
+        player_data[header.find("p").text] = header.find("h5").text
     overview_tables = pd.read_html(str(soup.find_all("table")))
     for table in overview_tables:
-        if set(table.columns) == set(ESPN_BOWLING_COLUMNS):
+        if "Wkts" in table.columns:
             player_data["bowling_summary"] = table
-        elif set(table.columns) == set(ESPN_BATTLING_COLUMNS):
+        elif "100s" in table.columns:
             player_data["batting_summary"] = table
 
     return player_data
--- CricinfoScrapers/test_player_data_scraper.py
+++ CricinfoScrapers/test_player_data_scraper.py
@@ -9,7 +9,7 @@ class TestPlayerDataScraper(unittest.TestCase):
         # self.assertEqual(get_player_data_from_player_id(player_id), expected_result)
         bumrah_id = 625383
         bumrah_data = get_player_data_from_player_id(bumrah_id)
-        bumrah_role = bumrah_data['role']
+        bumrah_role = bumrah_data['Playing Role']
         self.assertEqual(bumrah_role, 'Bowler')
 
 
